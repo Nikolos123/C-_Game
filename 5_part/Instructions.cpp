fab5801576d213f86bf3d45fcb7bfcc74225d313
Nikolos123 <Isponec2>
2021-12-23 18:02:18 +0300

    5 part

---
+++ 5_part/Instructions.cpp
@@ -0,0 +1,29 @@
+/*Nagorniy Nikolay
+ *
+ *
+ *
+ *  *vector<string>: :iterator mylterator (:: = Далее ставится оператор разрешения области видимости (символ : : )
+ */
+
+#include <iostream>
+
+
+
+using namespace std;
+void instructions();
+
+
+int main() {
+
+     instructions();
+
+    return 0;
+}
+
+void instructions(){
+
+    cout <<"Welcome to the most fun you've ever had with text!\n\n";
+    cout <<"Here's how to play the gae ..\n";
+}
+
+
